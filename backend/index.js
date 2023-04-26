61d977bf39702d77e39d8d05e830d20366af366a
boboPrem1 <boboprem1@gmail.com.com>
2023-04-26 17:49:43 +0000

    Added possibility to create a new user collection without authentification

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const connection = require("./db");
+const User = require("./models/userModel");
 // import dotenv
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const APP_URL_BASE = process.env.APP_URL_BASE ? process.env.APP_URL_BASE : "/";
+const API_URL_BASE = process.env.API_URL_BASE ? process.env.API_URL_BASE : "/";
 const URL_CONNECT_DEV = process.env.URL_CONNECT_DEV;
 
 // Middleware
@@ -16,28 +17,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes
-app.get(APP_URL_BASE, (req, res) => {
+app.get(API_URL_BASE, (req, res) => {
   res.json({ message: "Welcome to the API!" });
 });
 
 // generate all other user endpoints
-app.get(APP_URL_BASE + "users", (req, res) => {
+app.get(API_URL_BASE + "users", (req, res) => {
   res.json({ message: "Get all users" });
 });
 
-app.get(APP_URL_BASE + "users/:id", (req, res) => {
+app.get(API_URL_BASE + "users/:id", (req, res) => {
   res.json({ message: "Get user by id" });
 });
 
-app.post(APP_URL_BASE + "users", (req, res) => {
-  res.json({ message: "Create user" });
+app.post(API_URL_BASE + "users", async (req, res) => {
+  try {
+    if (req.body.username) {
+      const existingUsername = await User.findOne({ username: req.body.username });
+      const existingEmail = await User.findOne({ email: req.body.email });
+      const existingPhone = await User.findOne({ phone: req.body.phone });
+      const existing = existingUsername || existingEmail || existingPhone;
+      if (existing) {
+        res
+          .status(400)
+          .json({ message: "Existing user !" });
+      } else {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+      }
+    } else {
+      res.status(400).json({ message: "Bad Request !" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.put(APP_URL_BASE + "users/:id", (req, res) => {
+app.put(API_URL_BASE + "users/:id", (req, res) => {
   res.json({ message: "Update user" });
 });
 
-app.delete(APP_URL_BASE + "users/:id", (req, res) => {
+app.delete(API_URL_BASE + "users/:id", (req, res) => {
   res.json({ message: "Delete user" });
 });
 
