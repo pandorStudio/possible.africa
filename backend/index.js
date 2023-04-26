11728dd855eb1ecd079f621f623ca2e0d95bc0c1
boboPrem1 <boboprem1@gmail.com.com>
2023-04-26 17:21:10 +0000

    Created base endpoints for User Collection

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connection = require('./db');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connection = require("./db");
 // import dotenv
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const APP_URL_BASE = process.env.APP_URL_BASE;
+const APP_URL_BASE = process.env.APP_URL_BASE ? process.env.APP_URL_BASE : "/";
 const URL_CONNECT_DEV = process.env.URL_CONNECT_DEV;
 
 // Middleware
@@ -15,14 +15,36 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Route
+// Routes
 app.get(APP_URL_BASE, (req, res) => {
   res.json({ message: "Welcome to the API!" });
 });
 
+// generate all other user endpoints
+app.get(APP_URL_BASE + "users", (req, res) => {
+  res.json({ message: "Get all users" });
+});
 
+app.get(APP_URL_BASE + "users/:id", (req, res) => {
+  res.json({ message: "Get user by id" });
+});
+
+app.post(APP_URL_BASE + "users", (req, res) => {
+  res.json({ message: "Create user" });
+});
+
+app.put(APP_URL_BASE + "users/:id", (req, res) => {
+  res.json({ message: "Update user" });
+});
+
+app.delete(APP_URL_BASE + "users/:id", (req, res) => {
+  res.json({ message: "Delete user" });
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 connection(URL_CONNECT_DEV);
+
+// export app
+module.exports = app;
---
+++ backend/models/userModel.js
@@ -0,0 +1,59 @@
+const mongoose = ("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    gender: {
+      type: String,
+      enum: ["m", "f", "o"],
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    facebook_profile: {
+      type: String,
+    },
+    twitter_profile: {
+      type: String,
+    },
+    linkedin_profile: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
